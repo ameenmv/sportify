b0d7966a460531163839c996580ecb8d6b835606
Ameen <shehaaa4@gmail.com>
2025-10-04 08:39:54 +0300

    .

--- src/components/Offers.vue
+++ src/components/Offers.vue
@@ -17,17 +17,17 @@
           </p>
           <img
             class="absolute bottom-[-15px] right-[-25px]"
-            src="/vector1.png"
+            src="/Vector1.png"
             alt=""
           />
           <img
             class="absolute top-[-15px] left-[-25px]"
-            src="/vector4.png"
+            src="/Vector4.png"
             alt=""
           />
           <img
             class="absolute bottom-[0px] right-[5px]"
-            src="/vector3.png"
+            src="/Vector3.png"
             alt=""
           />
         </div>
